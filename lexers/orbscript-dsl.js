a59a7e595aafac455d8047855caae84eae01c6f9
dustinpfister <dustin.pfister@gmail.com>
2020-03-17 09:56:46 -0400

    expanded tokens and changed language

--- lexers/orbscript-dsl.js
+++ lexers/orbscript-dsl.js
@@ -1,14 +1,39 @@
 
 var langTokens = [{
         lexeme: 'base',
-        type: 'function',
-        regEx: /base\{[a-z]+\,\d+/
-    }
+        type: 'keyword',
+        regEx: /base /,
+        //regEx: /base\{[a-z]+\,\d+/
+    }, {
+        lexeme: 'cap',
+        type: 'keyword',
+        regEx: /cap /,
+        //regEx: /base\{[a-z]+\,\d+/
+    }, , {
+        lexeme: 'attack',
+        type: 'property',
+        regEx: /attack /,
+        //regEx: /base\{[a-z]+\,\d+/
+    }, , {
+        lexeme: 'speed',
+        type: 'property',
+        regEx: /speed /,
+        //regEx: /base\{[a-z]+\,\d+/
+    }, {
+        lexeme: 'number',
+        type: 'value',
+        regEx: /\d+/,
+        //regEx: /base\{[a-z]+\,\d+/
+    },
 ];
 
 // just export the lexer function
 module.exports = function (orbScript) {
     var tokens = [],
     lines = orbScript.split(';');
-    return lines[0].match(langTokens[0].regEx);
+    return lines.map(currentLine => {
+
+        return currentLine.match(langTokens[0].regEx);
+
+    });
 };
--- test-orbscript-1.js
+++ test-orbscript-1.js
@@ -1,5 +1,5 @@
 let orbScriptLexer = require('./lexers/orbscript-dsl.js');
 
-var tokens = orbScriptLexer('base{attack,30;base{speed,10');
+var tokens = orbScriptLexer('base attack 30;base speed 10;cap speed 30');
 
 console.log(tokens);
