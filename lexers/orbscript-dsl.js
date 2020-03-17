6c02d28f2033c38139ac96bf8e39e4a05bd8c073
dustinpfister <dustin.pfister@gmail.com>
2020-03-17 10:15:10 -0400

    getting token objects

--- lexers/orbscript-dsl.js
+++ lexers/orbscript-dsl.js
@@ -1,40 +1,59 @@
 
 var langTokens = [{
-        lexeme: 'base',
+        desc: 'base',
         type: 'keyword',
-        regEx: /base /,
+        regEx: /base/
         //regEx: /base\{[a-z]+\,\d+/
     }, {
-        lexeme: 'cap',
+        desc: 'cap',
         type: 'keyword',
-        regEx: /cap /,
-    }, , {
-        lexeme: 'attack',
-        type: 'property',
-        regEx: /attack /,
-    }, , {
-        lexeme: 'speed',
-        type: 'property',
-        regEx: /speed /,
+        regEx: /cap/
     }, {
-        lexeme: 'number',
+        desc: 'attack',
+        type: 'property',
+        regEx: /attack/
+    }, {
+        desc: 'speed',
+        type: 'property',
+        regEx: /speed/
+    }, {
+        desc: 'number',
         type: 'value',
-        regEx: /\d+/,
-    },
+        regEx: /\d+/
+    }
 ];
 
 // just export the lexer function
 module.exports = function (orbScript) {
     var tokens = [],
-    //tokenLen = tokens.length,
+    tokenLen = langTokens.length,
     lines = orbScript.split(';');
     return lines.map(currentLine => {
 
         let lineTokens = currentLine.split(' ');
 
-        return lineTokens.map((token) => {
+        return lineTokens.map((lexem) => {
 
-            return token;
+            let i = 0,
+            obj = {};
+            while (i < tokenLen) {
+
+                let t = langTokens[i];
+                m = lexem.match(t.regEx);
+
+                obj.lexem = lexem;
+                if (m) {
+
+                    obj.desc = t.desc;
+                    obj.type = t.type;
+
+                    break;
+                }
+
+                i += 1;
+            }
+
+            return obj;
 
         });
     });
