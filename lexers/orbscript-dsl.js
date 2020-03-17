b80c7b66219179a28e555c807c899db5ffce347e
dustinpfister <dustin.pfister@gmail.com>
2020-03-17 10:27:31 -0400

    lexer cleanup

--- lexers/orbscript-dsl.js
+++ lexers/orbscript-dsl.js
@@ -1,9 +1,8 @@
 
-var langTokens = [{
+const langTokens = [{
         desc: 'base',
         type: 'keyword',
         regEx: /base/
-        //regEx: /base\{[a-z]+\,\d+/
     }, {
         desc: 'cap',
         type: 'keyword',
@@ -24,41 +23,30 @@ var langTokens = [{
 ];
 
 // return an array of token objects for the given text line
-let lineToTokenObjects = function (line, lineNum) {
-
+let lineToTokenObjects = (line, lineNum) => {
     let lineTokens = line.split(' '),
     tokenLen = langTokens.length;
-
     return lineTokens.map((lexem) => {
-
         let i = 0,
         obj = {};
         while (i < tokenLen) {
-
             let t = langTokens[i];
             m = lexem.match(t.regEx);
-
             obj.lexem = lexem;
             if (m) {
-
                 obj.desc = t.desc;
                 obj.type = t.type;
-
                 break;
             }
-
             i += 1;
         }
-
         return obj;
-
     });
-
 };
 
 // just export the lexer function
-module.exports = function (orbScript) {
-    var tokens = [],
+module.exports = (orbScript) => {
+    let tokens = [],
     //tokenLen = langTokens.length,
     lines = orbScript.split(';');
     return lines.map((currentLine, i) => {
