3ec39fed40bfe0d997b43fd90f49563fbe2cd1ac
dustinpfister <dustin.pfister@gmail.com>
2020-03-17 10:46:43 -0400

    readme

--- test-orbscript-1.js
+++ test-orbscript-1.js
@@ -1,6 +1,5 @@
 let orbScriptLexer = require('./lexers/orbscript-dsl.js');
 
-//var tokens = orbScriptLexer('base attack 30;base speed 10;cap speed 30');
-var tokens = orbScriptLexer('base attack 30');
+let tokens = orbScriptLexer('base attack 30');
 
 console.log(JSON.stringify(tokens));
